d71eb2250c4d512746570dd68f914f7e28724de8
kplakosh <kplakosh@gmail.com>
2018-11-07 21:24:46 -0800

    logic for class open

--- shared.js
+++ shared.js
@@ -2,19 +2,35 @@ var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener("click", function () {
-        modal.style.display = "block";
-        backdrop.style.display = "block";
+        // modal.style.display = "block";
+        // backdrop.style.display = "block";
+        // modal.className = "open";  // This will overwrite the complete class list
+        modal.classList.add("open");
+        backdrop.classList.add("open");
     });
 }
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function() {
+    // mobileNav.style.display = "none";
+    mobileNav.classList.remove("open");
+    closeModal();
+});
 
 modalNoButton.addEventListener("click", closeModal);
 
 function closeModal () {
-    modal.style.display = "none";
-    backdrop.style.display = "none";
+    // modal.style.display = "none";
+    // backdrop.style.display = "none";
+    modal.classList.remove("open");
+    backdrop.classList.remove("open");
 }
+
+toggleButton.addEventListener("click", function() {
+    mobileNav.classList.add("open");
+    backdrop.classList.add("open");
+})
